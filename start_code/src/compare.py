da85c6bece2a8151be6b057676cba8107805f40d
Juan <jpalenzuela@outlook.com>
2020-11-25 23:31:01 +0000

    task finalised

--- start_code/src/compare.py
+++ start_code/src/compare.py
@@ -1,3 +1,9 @@
 def compare(number_1, number_2):
-    return f"{number_1} is greater than {number_2}"
+    if number_1 > number_2:
+        return f"{number_1} is greater than {number_2}"
+    elif number_1 < number_2:
+        return f"{number_1} is less than {number_2}"
+    else:
+        return 'both numbers are equal'
+
 
--- start_code/tests/compare_test.py
+++ start_code/tests/compare_test.py
@@ -5,3 +5,9 @@ class TestCompare(unittest.TestCase):
 
     def test_compare_3_1_returns_3_is_greater_than_1(self):
         self.assertEqual("3 is greater than 1", compare(3, 1))
+
+    def test_compare_5_10_returns_5_is_less_than_10(self):
+        self.assertEqual("5 is less than 10", compare(5, 10))
+
+    def test_compare_7_7_returns_equal(self):
+        self.assertEqual("both numbers are equal", compare(7, 7))
